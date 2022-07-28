601234ac025a14d6d57d66792193a0e5d7ddcbd3
tallez <allez@swift.ch>
2022-07-28 15:18:45 +0200

    compass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,21 +19,15 @@ export default function Home() {
 
       <main className="flex h-screen w-screen flex-col items-center justify-center bg-zinc-900 text-white">
         <iframe
-          className="h-1/2"
-          title="3d Animated Realistic Human Heart V1.0"
+          className="h-full w-full"
+          title="Compass"
           frameBorder="0"
           allow="autoplay; fullscreen; xr-spatial-tracking"
           xr-spatial-tracking
           execution-while-out-of-viewport
           execution-while-not-rendered
-          src="https://sketchfab.com/models/a70c0c47fe4b4bbfabfc8f445365d5a4/embed?autostart=1&preload=1"
-        ></iframe>
-        <iframe
-          className="h-1/2"
-          src="https://my.spline.design/organicmaterialcopy-fa4c3c90752b539162cb60b609a9182f/"
-          frameBorder="0"
-          width="100%"
-          height="100%"
+          web-share
+          src="https://sketchfab.com/models/e712ba658ab84d7791efba54d6f5c243/embed?autostart=1&preload=1"
         ></iframe>
       </main>
     </div>
