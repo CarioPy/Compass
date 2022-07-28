7791ab17cf1df01857ca1d35d187674860e9c944
tallez <allez@swift.ch>
2022-07-28 15:01:13 +0200

    initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 
 import Head from "next/head"
-import Link from "next/link"
 
 export default function Home() {
   return (
@@ -18,124 +17,41 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex min-h-screen justify-center bg-gradient-to-b from-gray-50 via-gray-50 to-gray-100 py-20">
-        <div>
-          <h1 className="px-5 text-center text-4xl font-bold leading-tight tracking-tight sm:mt-4 sm:text-6xl">
-            Next.js
-            <br />
-            Advanced Starter
-          </h1>
-
-          <h2 className="mx-auto mt-8 max-w-4xl px-10 text-center text-base tracking-tight text-gray-600 sm:text-2xl md:mt-5 md:text-2xl">
-            Tailwind CSS 3.0, ESLint & Prettier without a single line of config!
-            Easily extendable zero-config template for pros and beginners.
-          </h2>
-
-          <div className="px-4 sm:px-0">
-            <section
-              className="mt-6 grid min-h-[350px] w-full grid-cols-1 rounded-lg bg-white sm:mt-20 sm:min-w-[1000px] sm:grid-cols-2"
-              style={{
-                boxShadow: "rgba(0, 0, 0, 0.12) 0px 30px 60px 0px",
-              }}
-            >
-              <div className="flex flex-col justify-center rounded-l-lg bg-gray-50">
-                <FeatureList>
-                  <Feature main="Tailwind CSS">
-                    Fast design workflow with <InfoText text="Tailwind CSS" />
-                  </Feature>
-
-                  <Feature main="TypeScript">
-                    <InfoText text="TypeScript" /> by default
-                  </Feature>
-
-                  <Feature main="ESLint config">
-                    Customizable <InfoText text="ESLint config" />
-                  </Feature>
-
-                  <Feature main="Code formatting">
-                    <InfoText text="Code formatting" /> with Prettier
-                  </Feature>
-
-                  <Feature main="Absolute imports">
-                    Standardized <InfoText text="absolute imports" />
-                  </Feature>
-
-                  <Feature main="Absolute imports">
-                    Ready-to-go <InfoText text="Jest" /> setup
-                  </Feature>
-                </FeatureList>
-              </div>
-
-              <div className="space-y-5 place-self-center px-4 py-24 text-center">
-                <h3 className="text-3xl font-bold">Get it 👇</h3>
-
-                <span className="inline-flex rounded-md shadow-sm">
-                  <Link href="https://github.com/agcty/nextjs-advanced-starter">
-                    <a
-                      type="button"
-                      className="inline-flex items-center rounded-md border border-transparent bg-blue-600 px-4 py-4 font-medium leading-6 text-white transition duration-150 ease-in-out hover:bg-blue-500 focus:border-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-400 active:bg-blue-700 sm:px-10"
-                    >
-                      Copy Template from GitHub
-                    </a>
-                  </Link>
-                </span>
-              </div>
-            </section>
-            <p className="mt-6 text-center text-xs font-medium text-gray-600">
-              Built by{" "}
-              <a
-                className="font-medium text-blue-600 transition duration-150 ease-in-out hover:text-blue-500 focus:underline focus:outline-none"
-                href="https://twitter.com/agctyz"
-              >
-                @agctyz
-              </a>
-            </p>
-          </div>
+      <main className="flex h-screen w-screen items-center justify-center bg-zinc-900">
+        <div className="m-8 flex w-1/6 flex-col justify-start">
+          <p className="text-2xl text-white">Sketchfab</p>
+          <ul className="text-white">
+            <li>Easy to use</li>
+            <li>World leader owned by Epic Games</li>
+            <li>15€/month billed yearly</li>
+          </ul>
+        </div>
+        <iframe
+          className="h-1/2 w-1/3"
+          title="3d Animated Realistic Human Heart V1.0"
+          frameBorder="0"
+          allow="autoplay; fullscreen; xr-spatial-tracking"
+          xr-spatial-tracking
+          execution-while-out-of-viewport
+          execution-while-not-rendered
+          src="https://sketchfab.com/models/a70c0c47fe4b4bbfabfc8f445365d5a4/embed?autostart=1&preload=1"
+        ></iframe>
+        <iframe
+          className="h-1/2 w-1/3"
+          src="https://my.spline.design/organicmaterialcopy-fa4c3c90752b539162cb60b609a9182f/"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        ></iframe>
+        <div className="m-8 flex w-1/6 flex-col justify-start">
+          <p className="text-2xl text-white">Spline</p>
+          <ul className="text-white">
+            <li>Harder to use</li>
+            <li>Independant company</li>
+            <li>7$/month</li>
+          </ul>
         </div>
       </main>
     </div>
-  )
-}
-
-interface FeatureListProps {
-  children: React.ReactNode
-}
-
-function FeatureList({ children }: FeatureListProps) {
-  return <ul className="space-y-5 px-12 py-12">{children}</ul>
-}
-
-function Feature({ children, main }) {
-  return (
-    <li className="flex items-center">
-      <CheckIcon className="hiddden hidden h-5 w-5 flex-shrink-0 rounded-full bg-blue-600 p-1 text-gray-100 sm:inline" />
-      <p className="ml-3 hidden text-lg text-gray-600 sm:inline">{children}</p>
-
-      <p className="mx-auto sm:hidden">
-        <InfoText text={main} />
-      </p>
-    </li>
-  )
-}
-
-function InfoText({ text }) {
-  return (
-    <span className="inline-flex items-center rounded-md bg-gray-100 px-3 py-2 font-medium text-gray-700">
-      <CheckIcon className="mr-3 inline-flex h-5 w-5 flex-shrink-0 rounded-full bg-blue-600 p-1 text-gray-100 sm:hidden" />
-      {text}
-    </span>
-  )
-}
-
-function CheckIcon(props) {
-  return (
-    <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" {...props}>
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M5 13l4 4L19 7"
-      />
-    </svg>
   )
 }
